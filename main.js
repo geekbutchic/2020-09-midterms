3f246df57b595017507596fd2cfd99ce33ef5344
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-11-02 10:04:04 -0800

    working on solutions

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // Your code below.
+function isValidPassword(str) {
+  if (str.includes('.') || (str.includes(' ')) || (str.length < 12)) {
+      return false;
+  } else {
+      return true;
+  }
+}
 
+// returns false = no spaces 
+// returns false = no periods 
+// returns false = 12 characters long 
 
+const onlyCs = function (grades) {
+  const newGrades = [];
+  for (const grade of grades) {
+      if (grade < 79 && grade < 70 ) {
+          newGrades.push(grade);
+      }
+  }  
+  return newGrades;
+}
 
-
-
-
-
-
-
+// try array.filter(function(a){return a>=3&&a<=6}). for [1,2,3,3.1,4,6.1], it will return [3,3.1,4]
 
 
 // Our code below. Don't touch!
